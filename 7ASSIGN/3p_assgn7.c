a83885fa8d4ca61e80e12827d306dd6a51274446
1c3b3r6 <dipunj.mnnit@gmail.com>
2017-03-09 18:16:01 +0530

    Added full assignment 7

--- 7ASSIGN/3p_assgn7.c
+++ 7ASSIGN/3p_assgn7.c
@@ -2,58 +2,53 @@
 // LAB        : PROGRAMMING TOOLS - II
 // ASSIGNMENT : #7
 	
-// PROBLEM    : #1
+// PROBLEM    : #3
 
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
-void enQ(int )
-void sig_handler(int sig)
+void check_pending()
 {
-	switch (sig)
+	sigset_t set;
+	if( sigpending( &set ) != 0)
+		perror("sigpending() error");
+	else
 	{
-		case SIGINT:
-		{
-			enQ(SIGINT);
-			printf("SIGINT occured\n");
-			break;
-		}
-		case SIGHUP:
-		{
-			enQ(SIGHUP);
-			printf("SIGHUP occured\n");
-			break;
-		}
-		case SIGABRT:
-		{
-			enQ(SIGABRT);
-			printf("SIGABRT occured\n");
-			break;
-		}
+		if(sigismember(&set,SIGHUP))
+			printf("\t| SIG:1 | SIGHUP is pending\n");	
+		if(sigismember(&set,SIGINT))
+			printf("\t| SIG:2 | SIGINT is pending\n");
+		if(sigismember(&set,SIGABRT))
+			printf("\t| SIG:6 | SIGABRT is pending\n");
 	}
 }
+
 void install_handler()
 {
-	signal(SIGINT,sig_handler);
-	signal(SIGHUP,sig_handler);
-	signal(SIGTERM,sig_handler);
-
 	sigset_t block,empty;
-	sigemptyset(empty);
-	sigemptyset(block);
-	sigaddset(block, SIGINT);
-	sigaddset(block, SIGHUP);
-	sigaddset(block, SIGABRT);
+	sigemptyset(&empty);
+	sigemptyset(&block);
+	sigaddset(&block, SIGINT);
+	sigaddset(&block, SIGHUP);
+	sigaddset(&block, SIGABRT);
 
 	//BLOCKING SIGINT, SIGHUP,SIGABRT
-	sigprocmask(SIG_BLOCK,block,empty);
+	sigprocmask(SIG_BLOCK,&block,&empty);
 }
 void main()
 {
 	install_handler();
-	while(1)
+	printf("\t======= INSTRUCTIONS =======\n");
+	printf("This process once inititated would block SIGABRT, SIGINT, SIGHUP\n");
+	printf("To quit the process or send signals to this process, please open a new tty and find this process's PID by running the command \"ps -x\"\n");
+	printf("TO BEGIN PRESS 1\n");
+	char op = getchar();
+	while(op)
 		{
-			printf("Hello, this is main\n");
+			printf("\n_______________________________________________\n");
+			printf("Hello, this is main\n\tPENDING LIST:\n\n");
+			check_pending();
+			printf("_______________________________________________\n");
 			sleep(1);
 		}
 }
