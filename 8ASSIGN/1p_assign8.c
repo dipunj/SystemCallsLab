aa55db5c1ec14304360e77352c22a700643d248c
1c3b3r6 <dipunj.mnnit@gmail.com>
2017-03-24 12:37:51 +0530

    Added q1 of assgn 8

---
+++ 8ASSIGN/1p_assign8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define NUM 5
+
+void *printHello(void *m)
+{
+	int i;
+	int n = (int)m;
+	for(i=0;i<5;++i)
+		printf("Hello World (thread %ld)\n",n+1);
+	pthread_exit(NULL);
+}
+
+int main()
+{
+	int arg;
+	pthread_t thread[NUM];
+	for(arg = 0; arg < NUM ; ++arg)
+	{
+		printf("In main, creating thread #%d\n",arg+1);
+		int rc = pthread_create ( &thread[arg], NULL, printHello, (void *)arg);
+		if(rc)
+			perror("pthread_create()");
+	}
+	pthread_exit(NULL);
+}
